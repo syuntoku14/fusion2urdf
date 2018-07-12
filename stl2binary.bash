36dce9684858da90e7de65c710e6862078f535db
Toshinori Kitamura <syuntoku14@keio.jp>
2018-07-13 03:12:10 +0900

    Update and rename stl2urdf.bash to stl2binary.bash

--- stl2binary.bash
+++ stl2binary.bash
@@ -1,7 +1,4 @@
 #!/usr/bin/env/bash
-# Put all stl files that you want to convert to binary into "mm_stl". 
-# Also, add a joint information at joint_info/joints.py.
-# Then, run this script.
 
 for filename in ./mm_stl/*.stl; do python mm2m_stl.py $filename; done
 for filename in ./mm_stl/*_m.stl; do ruby convertSTL.rb $filename; done
@@ -11,5 +8,3 @@ mkdir m_stl bin_stl
 rm *_m_m.stl
 mv ./mm_stl/*_m.stl ./m_stl
 mv ./mm_stl/*-binary.stl ./bin_stl
-
-python ./joint_info/joints2urdf.py
